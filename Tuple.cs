496d8929cb6224a3f4222f70055d934fa2df3c04
jerryrox <jerryrox@live.co.kr>
2018-07-08 19:17:10 +1200

    Small update
    - Added Readme content
    - Support Tuple<T1,T2,T3>

--- Tuple.cs
+++ Tuple.cs
@@ -22,6 +22,10 @@ namespace System {
 		internal static int CombineHashCodes(int h1, int h2) {
 			return (((h1 << 5) + h1) ^ h2);
 		}
+
+		internal static int CombineHashCodes(int h1, int h2, int h3) {
+			return CombineHashCodes(CombineHashCodes(h1, h2), h3);
+		}
 	}
 
 	[Serializable]
@@ -78,7 +82,7 @@ namespace System {
 	}
 
 	[Serializable]
-	public class Tuple<T1, T2> {
+	public class Tuple<T1, T2> : IComparable {
 
 		public readonly T1 Item1;
 		public readonly T2 Item2;
@@ -140,6 +144,80 @@ namespace System {
 			c = Comparer.Default.Compare(this.Item1, other.Item1);
 			if(c != 0) return c;
 			return Comparer.Default.Compare(this.Item2, other.Item2);
+		}
+	}
+
+	[Serializable]
+	public class Tuple<T1, T2, T3> : IComparable {
+
+		public readonly T1 Item1;
+		public readonly T2 Item2;
+		public readonly T3 Item3;
+		public readonly int Length = 3;
+
+
+		public object this[int index] {
+			get {
+				switch(index) {
+				case 0: return Item1;
+				case 1: return Item2;
+				case 2: return Item3;
+				default: throw new IndexOutOfRangeException();
+				}
+			}
+		}
+
+
+		public Tuple(T1 t1, T2 t2, T3 t3)
+		{
+			Item1 = t1;
+			Item2 = t2;
+			Item3 = t3;
+		}
+
+		public override bool Equals (object obj)
+		{
+			if(obj == null) return false;
+			Tuple<T1,T2,T3> other = obj as Tuple<T1,T2,T3>;
+			if(other == null) return false;
+			return EqComparer.Default.Equals(this.Item1, other.Item1) &&
+				EqComparer.Default.Equals(this.Item2, other.Item2) &&
+				EqComparer.Default.Equals(this.Item3, other.Item3);
+		}
+
+		public override string ToString ()
+		{
+			StringBuilder sb = new StringBuilder();
+			sb.Append('(')
+				.Append(Item1).Append(", ")
+				.Append(Item2).Append(", ")
+				.Append(Item3)
+				.Append(')');
+			return sb.ToString();
+		}
+
+		public override int GetHashCode ()
+		{
+			var comparer = EqComparer.Default;
+			return Tuple.CombineHashCodes(
+				comparer.GetHashCode(Item1),
+				comparer.GetHashCode(Item2),
+				comparer.GetHashCode(Item3)
+			);
+		}
+
+		public int CompareTo(object obj)
+		{
+			if(obj == null) return 1;
+			Tuple<T1,T2,T3> other = obj as Tuple<T1,T2,T3>;
+			if(other == null)
+				throw new ArgumentException("Argument type invalid", "obj");
+			int c = 0;
+			c = Comparer.Default.Compare(this.Item1, other.Item1);
+			if(c != 0) return c;
+			c = Comparer.Default.Compare(this.Item2, other.Item2);
+			if(c != 0) return c;
+			return Comparer.Default.Compare(this.Item3, other.Item3);
 		}
 	}
 }
